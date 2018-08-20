ff2737b98b017c4168e350591dd8af7b76dde8bf
WSudul <wojciech.sudul@gmail.com>
2018-08-21 01:07:39 +0200

    adding PaymentServiceTest draft

---
+++ src/test/java/lot/service/PaymentServiceTest.java
@@ -0,0 +1,26 @@
+package lot.service;
+
+import org.junit.Test;
+
+public class PaymentServiceTest {
+    @Test
+    public void processPayment_ReturnsTrue_WhenUserHasFunds() throws Exception {
+    }
+
+    @Test
+    public void processPayment_ReturnsFalse_WhenUserHasNoFunds() throws Exception {
+    }
+
+    @Test
+    public void processPayment_ReturnsFalse_WhenEntryWasAlreadyPaid() throws Exception {
+    }
+
+    @Test
+    public void calculatePaymentValue_ReturnsNull_WhenGivenAlreadyPaidEntry() throws Exception {
+    }
+
+    @Test
+    public void calculatePaymentValue_ReturnsCorrectValue_WhenGivenUnpaidEntry() throws Exception {
+    }
+
+}
