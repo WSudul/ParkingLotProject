42fd916b365a2ddc6707b265d59903ee83885209
WSudul <wojciech.sudul@gmail.com>
2018-09-01 00:59:26 +0200

    add Lot object + repository

---
+++ src/main/java/lot/repository/LotRepository.java
@@ -0,0 +1,14 @@
+package lot.repository;
+
+
+import lot.model.Lot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LotRepository extends JpaRepository<Lot, String> {
+
+    Lot findOne();
+
+
+}
