de9309796cfef7ee759cc6e559166a41da770168
WSudul <wojciech.sudul@gmail.com>
2018-10-04 23:35:49 +0200

    add user update method in controller

--- src/main/java/lot/controller/UserController.java
+++ src/main/java/lot/controller/UserController.java
@@ -1,11 +1,13 @@
 package lot.controller;
 
+import lot.model.UpdateUserData;
 import lot.model.User;
 import lot.model.UserRegistrationData;
 import lot.service.UserService;
 import lot.utils.GenericResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -46,6 +50,29 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(genericResponse);
         else
             return ResponseEntity.created(null).body(genericResponse);
+
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public ResponseEntity<GenericResponse> addPlate(@Valid @RequestBody UpdateUserData updateUserData,
+                                                    @AuthenticationPrincipal Principal principal) {
+        GenericResponse genericResponse = new GenericResponse();
+
+
+        String name = principal.getName();
+
+        Optional<User> user = userService.findUser(name);
+
+        if (user.isPresent()) {
+            if (false == userService.updateUser(user.get(), updateUserData)) ;
+        } else {
+            genericResponse.getErrors().addError("Update failed");
+        }
+
+        if (genericResponse.getErrors().hasErrors())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(genericResponse);
+        else
+            return ResponseEntity.status(HttpStatus.OK).body(genericResponse);
 
     }
 }
---
+++ src/main/java/lot/model/UpdateUserData.java
@@ -0,0 +1,46 @@
+package lot.model;
+
+
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
+
+public class UpdateUserData {
+
+    @NotEmpty
+    private String nickname;
+    private Set<String> addPlates;
+    private Set<String> removePlates;
+
+    public UpdateUserData() {
+    }
+
+    public UpdateUserData(@NotEmpty String nickname, Set<String> addPlates, Set<String> removePlates) {
+        this.nickname = nickname;
+        this.addPlates = addPlates;
+        this.removePlates = removePlates;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Set<String> getAddPlates() {
+        return addPlates;
+    }
+
+    public void setAddPlates(Set<String> addPlates) {
+        this.addPlates = addPlates;
+    }
+
+    public Set<String> getRemovePlates() {
+        return removePlates;
+    }
+
+    public void setRemovePlates(Set<String> removePlates) {
+        this.removePlates = removePlates;
+    }
+}
