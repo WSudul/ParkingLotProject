3de6337a7c092f071807a5209338cccd3360f3c9
WSudul <wojciech.sudul@gmail.com>
2019-06-25 01:03:36 +0200

    add user list to index mapping

--- src/main/java/lot/controller/HomeMVCController.java
+++ src/main/java/lot/controller/HomeMVCController.java
@@ -1,14 +1,23 @@
 package lot.controller;
 
+import lot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/")
 public class HomeMVCController {
+
+    @Autowired
+    UserService userService;
+
+
     @RequestMapping(value = {"", "/index", "home"}, method = RequestMethod.GET)
-    String returnIndex() {
+    String returnIndex(Model model) {
+        model.addAttribute("users", userService.findAllUsers());
         return "index";
     }
 }
